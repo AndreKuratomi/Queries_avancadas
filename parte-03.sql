6284e036b92486c08fbe485473209bc00ea13420
André Kuratomi <andrekuratomi@gmail.com>
2021-11-21 13:05:53 -0300

    refactor: notation of doubts at parte-03.sql

--- parte-03.sql
+++ parte-03.sql
@@ -8,46 +8,46 @@ select e.rua, e.pais, e.cidade, u.id, u.nome, u.email, u.senha, u.endereco_id
 	on u.endereco_id = e.id
 	order by u.endereco_id asc;
 
---3??
+--3?? (dúvida em como deixar o select igual ao modelo. Está semelhante à da table usuario_redes_sociais e tentei incluir sem sucesso)
 select r.nome, u.id, u.nome, u.email, u.senha, u.endereco_id 
 	from redes_sociais as r
 	full outer join usuario as u
-	on r.id = u.id
-	order by usuario_redes_sociais.redes_sociais_id from usuario_redes_sociais urs;
+	on r.id = u.id;
+	--order by usuario_redes_sociais.redes_sociais_id from usuario_redes_sociais urs;
 
---4??
+--4?? (dúvida em como fazer o join de mais de 2 tabelas.)
 select r.id, r.nome, u.id, u.nome, u.email, u.senha, u.endereco_id, e. id, e.rua, e.pais, e.cidade
 	from redes_sociais as r 
 	full outer join usuario as u
 	full outer join enderecos as e
 	on r.id = u.id and u.endereco_id = e.id;
 
---5??
+--5?? (idem seletor 4)
 select 
 	rede_social.nome, 
-	usuario.nome, usuario.email, usuario.id,
+	usuario.nome, usuario.email,
 	e.cidade 
 	from redes_sociais as rede_social
 	full outer join enderecos as e
-	on rede_social.id = usuario.id and uusuario.endereco_id = e.id;
+	on rede_social.id = usuario.id and usuario.endereco_id = e.id;
 
---6
+--6 (consequência selector 5)
 (...)
 where r.nome = "Youtube";
 
---7
+--7 (idem)
 (...)
 where r.nome = "Instagram";
 
---8
+--8 (idem)
 (...)
 where r.nome = "Facebook";
 
---9
+--9 (idem)
 (...)
 where r.nome = "Tiktok";
 
---10
+--10 (idem)
 (...)
 where r.nome = "Twitter";
 
